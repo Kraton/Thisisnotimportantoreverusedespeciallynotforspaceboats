58743a80f18af9f2c36574a22daf1819fe44f9e4
Kraton <mic14@hotmail.co.uk>
2015-02-24 22:12:47 +0000

    fixed AI flight logic, will not rotate and track player ship anywhere on map

--- AIShip.cpp
+++ AIShip.cpp
@@ -6,7 +6,8 @@
 
 AIShip::AIShip() : 
 _velocity(100.0f),
-_maxVelocity(600.0f)
+_maxVelocity(600.0f),
+_angle(0.0f)
 {
 	Load(AI_ship_image);
 	assert(IsLoaded());
@@ -33,10 +34,10 @@ void AIShip::Update(float elapsedTime)
 	float moveByX = 1;
 	float moveByY = 1;
 	int desired_turning_speed_per_second = 90;
-	printf("velocity is %f", _velocity);
-	printf("elapsed Time is %f \n", elapsedTime);
+//	printf("velocity is %f", _velocity);
+//	printf("elapsed Time is %f \n", elapsedTime);
 	float moveAmount = _velocity * elapsedTime;
-	printf("moveAmount is %f \n", moveAmount);
+//	printf("moveAmount is %f \n", moveAmount);
 
 	std::vector<std::string> vector = Game::GetGameObjectManager().getGameObjectNames();
 
@@ -50,59 +51,68 @@ void AIShip::Update(float elapsedTime)
 
 			float playerx =player1->GetPosition().x;
 			float playery =player1->GetPosition().y;
-			printf("playerx is %f \n", playerx);
-			printf("playery is %f \n", playery);
+			//printf("playerx is %f \n", playerx);
+			//printf("playery is %f \n", playery);
 
 			float AIx = GetPosition().x;
 			float AIy = GetPosition().y;
-			printf("AIx is %f \n", AIx);
-			printf("AIy is %f \n", AIy);
+			//printf("AIx is %f \n", AIx);
+			//printf("AIy is %f \n", AIy);
 			float angle_to_player = 0;
 
 			//using sohcahtoa to calcuate angle between ai ship and player
-			if( std::abs(AIy-playery) < std::abs(AIx - playerx))
+			if( AIx>playerx && AIy < playery)
 			{
-				printf("using x is the biggest gap, so y is opposite \n");
-				angle_to_player = (atan((std::abs(AIy-playery))/std::abs(AIx - playerx)))* 180/pi_value + 90; //90 to accomdate for angle defaults
+				//printf("player is bottom left of AI");
+				angle_to_player = 90 - (atan((std::abs(AIy-playery))/std::abs(AIx - playerx)))* 180/pi_value; //90 to accomdate for angle defaults
+				//printf("pure angle to player is%f \n", angle_to_player);
+				angle_to_player = -1*(180-angle_to_player);
+				//printf("modified angle to player is%f \n", angle_to_player);
 			}
-			else if( std::abs(AIy-playery) >= std::abs(AIx - playerx))
+			else if( AIx>playerx && AIy > playery)
 			{
-				printf("using y is the biggest gap so x is opposite \n");
-				angle_to_player = (atan(std::abs(AIx - playerx)/(std::abs(AIy-playery))))* 180/pi_value +90 ;//90 to accomdate for angle defaults
+				//printf("player is top left of AI");
+				angle_to_player = 90 - (atan((std::abs(AIy-playery))/std::abs(AIx - playerx)))* 180/pi_value; //90 to accomdate for angle defaults
+				//printf("pure angle to player is%f \n", angle_to_player);
+				angle_to_player = -1*(angle_to_player);
+				//printf("modified angle to player is%f \n", angle_to_player);
+			}
+			else if( AIx<playerx && AIy > playery)
+			{
+				//printf("player is top right of AI");
+				angle_to_player = 90 - (atan((std::abs(AIy-playery))/std::abs(AIx - playerx)))* 180/pi_value; //90 to accomdate for angle defaults
+				//printf("pure angle to player is%f \n", angle_to_player);
+				angle_to_player = (angle_to_player);
+				//printf("modified angle to player is%f \n", angle_to_player);
+			}
+			else if( AIx<playerx && AIy < playery)
+			{
+				//printf("player is bottom right of AI");
+				angle_to_player = 90 - (atan((std::abs(AIy-playery))/std::abs(AIx - playerx)))* 180/pi_value; //90 to accomdate for angle defaults
+				//printf("pure angle to player is%f \n", angle_to_player);
+				angle_to_player = (180 - angle_to_player);
+				//printf("modified angle to player is%f \n", angle_to_player);
 			}
 
-			printf("angle to player is%f \n", angle_to_player);
 
 			float linVelX = LinearVelocityX(angle_to_player);
 			float linVelY = LinearVelocityY(angle_to_player);
 
-			printf("linVelX is %f \n", linVelX);
-			printf("linVelY is %f \n", linVelY);
+			moveByX = linVelX * moveAmount ;
+			moveByY = linVelY * moveAmount ;
 
-			if(AIx-playerx >0){
-			moveByX = linVelX * moveAmount *-1;
-			}
-			else if(AIx - playerx <=0)
-			{
-				moveByX = linVelX * moveAmount ;
-			}
-
-			if(AIy-playery >0){
-			moveByY = linVelY * moveAmount *-1;
-			}
-			else if(AIy - playery <=0)
-			{
-				moveByY = linVelY * moveAmount ;
-			}
-
-			printf("moveby x is %f \n", moveByX);
-			printf("moveby Y is %f \n", moveByY);
 
 			if(p1BoundaryBox.Intersects(GetBoundingRect()))
 			{
 				SetPosition(starting_AI_loc);
 				printf(" got you ");
 			}
+			float rotate_angle= _angle-angle_to_player;
+			//printf("rotate angle is %f \n", rotate_angle);
+			//printf("angle is %f \n", _angle);
+			GetSprite().Rotate(rotate_angle);
+
+			_angle=angle_to_player;
 
 		}
 	}
